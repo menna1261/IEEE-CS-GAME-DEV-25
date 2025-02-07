135a9988337dcb465456d59dbaa963005d100922
Menna Jaheen <118180052+menna1261@users.noreply.github.com>
2025-02-07 23:27:37 +0200

    Update PipeMovement.cs

--- Task_1/Flappy bird game/Scripts/PipeMovement.cs
+++ Task_1/Flappy bird game/Scripts/PipeMovement.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class PipeMovement : MonoBehaviour
 {
-    public float movementSpeed = 5f;  // Speed at which the pipes move
-    private float DeadZone = -15f;    // The position where pipes will be destroyed
-
-    // Update is called once per frame
+    public float movementSpeed = 5f; 
+    private float DeadZone = -15f;    
+   
     void Update()
     {
-        // Move the pipe to the left
         transform.position += Vector3.left * movementSpeed * Time.deltaTime;
-
-        // Destroy the pipe when it reaches the DeadZone
         if (transform.position.x < DeadZone)
         {
             Destroy(gameObject);
